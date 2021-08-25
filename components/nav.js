a21d253140d2964162218a51b9a8bab222c67f8a
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-08-24 17:52:31 -0700

    add text decoration to footer

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import Nav from "../styles/components/nav";
 export default function nav() {
   return (
-    <nav>
+    <Nav>
       <Link href="/">Home</Link>
       <Link href="/book-consult">Book A Visit</Link>
       <Link href="/about">About</Link>
       <Link href="/staff-bios">Our Team</Link>
       <Link href="/faq">FAQ</Link>
-    </nav>
+    </Nav>
   );
 }
---
+++ styles/components/nav.js
@@ -0,0 +1,4 @@
+import styles from "./nav.module.css";
+export default function Nav({ children }) {
+  return <div className={styles.nav}>{children}</div>;
+}

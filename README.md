a870636d6cd01587a4a5d0a00e0152883864bc10
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-08-30 14:06:31 -0700

    drop down functionality

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
     - FAQ
     - Book an appointment via phone call
     - Bios
+
+
+    ## Collapsible Nav
+    - cannot seem to use window.innerWidth in Next  .. ah but it hasn't loaded yet ... make it async
+
+    - so if the screen size is small, display the hamburger and hide the main nav
+        - if the reveal, show the drop down
+            - else hide the dropdown but show the hamburger
+    - if the screen size is large, display the main nav and hide the hamburger
--- components/nav.js
+++ components/nav.js
@@ -1,11 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Nav from "../styles/components/nav";
+
 export default function nav() {
-  const [navView, setNavView] = useState(false);
-  const toggleNav = () => {
-    alert("hanga banga");
+  const [size, setSize] = useState(0);
+  const [style, setStyle] = useState("small");
+  const [reveal, setReveal] = useState(false);
+
+  const checkSize = async () => {
+    let s = await window.innerWidth;
+    if (s < 600) {
+      console.log(`small screen`, s);
+      setStyle("small");
+      // pass a style to the component
+    } else {
+      console.log(`big screen`, s);
+      setStyle("big");
+    }
+    setSize(s);
   };
+
+  useEffect(() => {
+    checkSize();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, [size]);
+
+  const toggleNav = () => {
+    setReveal(!reveal);
+  };
+  // return (
+  //   <>
+  //     <h1 className={style}>window</h1>
+  //     <h2>{size} PX</h2>
+  //     <style jsx>{`
+  //       .small {
+  //         color: red;
+  //       }
+  //     `}</style>
+  //   </>
+  // );
+
   return (
     <Nav>
       <ul>
@@ -20,6 +60,40 @@ export default function nav() {
         <span></span>
         <span></span>
       </div>
+      <div
+        style={{
+          display: reveal ? "block" : "none",
+          // backgroundColor: "black",
+          // float: "right",
+        }}
+      >
+        <ul className={style}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/book-consult">Book A Visit</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/staff-bios">Our Team</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/faq">FAQ</Link>
+          </li>
+        </ul>
+      </div>
+      <style jsx>{`
+       
+        .dropDown {
+          background-color: black;
+        }
+      `}</style>
     </Nav>
   );
 }
